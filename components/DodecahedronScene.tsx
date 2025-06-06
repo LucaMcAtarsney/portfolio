009b56008c5c15ae71ffd6d4ba173eaf79a21f8e
Luca McAtarsney <lucamc1604@gmail.com>
2025-06-06 09:45:07 +0100

    fixed dodeca

--- components/DodecahedronScene.tsx
+++ components/DodecahedronScene.tsx
@@ -1,19 +1,35 @@
 'use client'
 
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Dodecahedron } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Mesh } from 'three'
+
+function RotatingDodecahedron() {
+  const ref = useRef<Mesh>(null)
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.y += delta * 0.5
+      ref.current.rotation.x += delta * 0.3
+    }
+  })
+
+  return (
+    <mesh ref={ref}>
+      <dodecahedronGeometry args={[1, 0]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  )
+}
 
 export default function DodecahedronScene() {
   return (
     <div className="w-full h-screen">
       <Canvas camera={{ position: [2, 2, 2] }}>
         <ambientLight intensity={0.6} />
-        <hemisphereLight color="#ffffff" groundColor="#ffffff" intensity={0.6} />
         <directionalLight position={[10, 5, 3]} />
-        <Dodecahedron args={[1, 0]}>
-          <meshStandardMaterial color="white" />
-        </Dodecahedron>
-        <OrbitControls />
+        <RotatingDodecahedron />
+        {/* No OrbitControls! */}
       </Canvas>
     </div>
   )
